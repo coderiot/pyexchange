518888f9a06bc3219d33f6569b4b1fc1f3c24f91
peterrr <peterrr@github>
2013-11-06 22:51:38 +0100

    add first tests for Bitcurex and Bitfinex exchanges

--- pyexchange/tests.py
+++ pyexchange/tests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import decimal
+
 import unittest
 
 import pyexchange
@@ -46,6 +48,45 @@ class TestExchangesList(unittest.TestCase):
         for ex in pyexchange.exchanges():
             ex_obj = pyexchange.new_exchange(ex)
             self.assertIsInstance(ex_obj, models.Exchange)
+
+    def test_unknow_exchange(self):
+        #ex = pyexchange.new_exchange('unknown')
+        self.assertRaises(Exception, pyexchange.new_exchange, 'unknown')
+
+
+class TestBitcurex(unittest.TestCase):
+    """Test case docstring"""
+
+    def setUp(self):
+        self.ex = pyexchange.new_exchange('bitcurex')
+
+    def test_markets(self):
+        exp_markets = ['btc_eur', 'btc_pln']
+        obj_markets = self.ex.markets()
+        module_markets = pyexchange.bitcurex.markets()
+        self.assertItemsEqual(exp_markets, obj_markets)
+        self.assertItemsEqual(exp_markets, module_markets)
+
+    def test_ticker(self):
+        ticker = self.ex.ticker()
+        self.assertIsInstance(ticker, models.Ticker)
+        for k, v in ticker._asdict().items():
+            self.assertIsInstance(v, decimal.Decimal)
+
+
+class TestBitfinex(unittest.TestCase):
+    """Test case docstring"""
+
+    def setUp(self):
+        self.ex = pyexchange.bitfinex.Bitfinex()
+
+    def test_markets(self):
+        exp_markets = ['ltc_btc', 'ltc_usd', 'btc_usd']
+        obj_markets = self.ex.markets()
+        module_markets = pyexchange.bitfinex.markets()
+        self.assertItemsEqual(exp_markets, obj_markets)
+        self.assertItemsEqual(exp_markets, module_markets)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
